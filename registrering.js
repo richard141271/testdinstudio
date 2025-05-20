4877bb0e72168c83fda1193caa28279e2fb8a8a5
richard141271 <68450987+richard141271@users.noreply.github.com>
2025-05-20 23:32:26 +0200

    Add files via upload

--- registrering.js
+++ registrering.js
@@ -1,26 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const skjema = document.getElementById("registreringsskjema");
+  const skjema = document.getElementById("registreringsSkjema");
   if (skjema) {
     skjema.addEventListener("submit", e => {
       e.preventDefault();
-      const data = {
-        navn: document.getElementById("fornavn").value + " " + document.getElementById("etternavn").value,
+      const pass1 = document.getElementById("passord").value;
+      const pass2 = document.getElementById("gjentaPassord").value;
+      if (pass1 !== pass2) {
+        document.getElementById("registreringMelding").textContent = "Passordene matcher ikke.";
+        return;
+      }
+
+      const bruker = {
+        fornavn: document.getElementById("fornavn").value,
+        etternavn: document.getElementById("etternavn").value,
         epost: document.getElementById("epost").value,
         telefon: document.getElementById("telefon").value,
         vervetAv: document.getElementById("vervetAv").value,
         kontonummer: document.getElementById("kontonummer").value,
-        passord: document.getElementById("passord").value,
+        registrert: new Date().toLocaleString()
       };
-      if (!document.getElementById("godtaVilkår").checked) {
-        document.getElementById("registreringsmelding").innerText = "⚠️ Du må godta vilkårene.";
-        return;
-      }
-      if (data.passord !== document.getElementById("passord2").value) {
-        document.getElementById("registreringsmelding").innerText = "⚠️ Passordene matcher ikke.";
-        return;
-      }
-      localStorage.setItem("registrertBruker", JSON.stringify(data));
-      document.getElementById("registreringsmelding").innerText = "✅ Du er nå registrert!";
+      localStorage.setItem("registrertBruker", JSON.stringify(bruker));
+      window.location.hash = "#profil";
       visModul('modul-profil');
     });
   }
